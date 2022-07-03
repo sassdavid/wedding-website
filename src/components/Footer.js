6f9227a8b09211eccffa025240ded175be608ba1
Donald W. Boulton <donaldboulton@gmail.com>
2022-07-03 09:03:57 -0500

    update react 18.2.0 gatsby 4.17.2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -27,7 +27,7 @@ const Footer = (props) => (
             }}          
           />
         </span>{" "} 
-        <a href="/privacy">Privacy</a> Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js v4.11.0</a></p>
+        <a href="/privacy">Privacy</a> Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js v4.17.2</a></p>
     </footer>
 )
 
